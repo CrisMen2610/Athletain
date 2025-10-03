fd370bf0d87206198d8d8ea7a11bff3b95663719
CrisMen2610 <crisdamencast@gmail.com>
2025-10-02 23:08:42 -0500

    fix(router): modify router

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Header from "./layouts/header/Header";
 import Home from "./pages/Home/Home";
 import Footer from "./layouts/footer/Footer";
 import AboutUs from "@pages/about-us/About-Us";
 
 const App: React.FC = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Header />
     <Routes>
-      <Route path="/Athletain" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/sobre_nosotros" element={<AboutUs />} />
-      {/* Ruta para páginas no encontradas */}
       <Route path="*" element={<div>Página no encontrada</div>} />
     </Routes>
     <Footer />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default App;
